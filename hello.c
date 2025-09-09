3273f069878d8fb58e8680b5469085d1cd196841
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-09 08:36:20 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main(){
+
+    //1.hello world
+    printf("Hello world \n");
+
+
+    //2.Print your name 
+   printf("My Name is Janhavi \n");
+
+
+  //3.Print integer
+     int i=2;
+    printf("Value of i:%d \n",i);
+
+
+    //4.Print Two integer
+    int x=2, y=5;
+    printf("x=%d ,y=%d \n",x ,y);
+
+
+  //5.Print float value
+  float pi=3.14;
+  printf("Value of Pi=%f \n",pi);
+
+  //6.Print Character
+  char Grade='A';
+  printf("Grade is:%c \n",Grade);
+
+
+  //7.Simple addition
+     int a=3, b=6;
+  printf("Addition is=%d \n",a+b);
+
+
+  //8.Simple Subtraction
+  int c=3, d=6;
+  printf("subtraction is=%d \n",c-d);
+
+
+  //9.Simple Multipication
+  int e=3, f=6;
+  printf("Multipication is=%d \n",e*f);
+
+
+  //10.Simple Division
+  int g=3, h=6;
+  printf("Division is=%d \n",h/g); 
+
+    return 0;
+    
+
+
+    
+}
+
+
+
