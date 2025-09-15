f7cae86c2ca517b1dc6f9cf3b2a5aaa61d1510cf
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-15 18:19:45 +0530

    Add files via upload

---
+++ area-sq.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    int height, base, area;
+    printf("Enter Height=",height);
+    scanf("%d",&height);
+
+    printf("Enter Base= ",base);
+    scanf("%d",&base);
+
+    area=height*base;
+    printf("Area of sqaure =%d",area);
+
+
+
+}
