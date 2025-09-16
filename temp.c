42d72d3df7ad44c516e3d6c932c3bf9e59789ce9
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-16 08:43:10 +0530

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    int A, B, Sum;
+    printf("Enter temp for 1st time: ",A);
+    scanf("%d",&A);
+
+    printf("Enter temp for 2nd time: ",B);
+    scanf("%d",&B);
+
+    Sum=A+B;
+    printf("Final temp is%d: \n",Sum);
+  
+  
+
+}
