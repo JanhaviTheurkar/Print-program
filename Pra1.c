f5dc62c479061d4436842391042188ca09d0ecc8
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-27 19:22:18 +0530

    taking input from user to print name

---
+++ Pra1.c
@@ -0,0 +1,15 @@
+       
+       #include<stdio.h>
+       int main(){
+       int a[20];
+    
+       printf("Enter your : ");
+       scanf("%s",&a);
+
+       printf("Hello, Robotics! \n");
+
+       printf("Welcome %s",a);
+ 
+       return 0;
+
+       }
